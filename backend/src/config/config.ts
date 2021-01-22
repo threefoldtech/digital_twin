eb7344e53a9db24d88fdf6258085979ef2df8945
Jonas Wijne <jonas.wijne@jimber.org>
2021-01-22 17:35:41 +0100

    :sparkles: add prod config

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,7 +1,7 @@
 let userid = process.env.USER_ID || 'jonasw';
 export const config = {
-    appBackend: 'https://login.staging.jimber.org',
-    kycBackend: 'https://openkyc.staging.jimber.org',
+    appBackend: 'https://login.threefold.me',
+    kycBackend: 'https://openkyc.live',
     appId: `${userid}.digitaltwin.jimbertesting.be`,
     seedPhrase: 'calm science teach foil burst until next mango hole sponsor fold bottom cousin push focus track truly tornado turtle over tornado teach large fiscal',
     userid,
