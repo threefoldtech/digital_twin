31ac97153991f0cbe1bddc3c8dcd2b8ae4fe82e6
Ivan Coene <ivan@ivancoene.be>
2021-02-09 10:48:47 +0100

    Showing group members in team chat

--- frontend/src/components/GroupManagement.vue
+++ frontend/src/components/GroupManagement.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-white p-2 w-full h-52 relative rounded-lg mb-4 mt-0 md:grid place-items-center grid-cols-1"
+    class="bg-white p-2 w-full relative rounded-lg mb-4 mt-0 md:grid place-items-center grid-cols-1"
   >
     <div v-for="contact in groupChat.contacts" :key="contact.id">
       <div class="chatcard relative grid grid-cols-12 rounded-lg mb-2 py-2">
@@ -14,7 +14,7 @@
             }"
           ></div>
         </div>
-        <div class="md:col-span-10 col-span-9 pl-2">
+        <div class="md:col-span-9 col-span-8 pl-2">
           <p class="flex place-content-between">
             <span class="font-bold">
               {{ contact.id }}
@@ -30,12 +30,13 @@
 import { computed } from "vue";
 import { statusList } from "@/store/statusStore";
 import { GroupChat } from "../types/index";
+import AvatarImg from "@/components/AvatarImg.vue"
 export default {
   name: "GroupManagementBlock",
   props: {
     groupChat: { required: true },
   },
-  
+  components: {AvatarImg},
   setup(props) {
     const removeFromGroup = () => {
     
