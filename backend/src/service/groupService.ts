8b4d4081da1cca0ba73047a6748a29b5b2654869
Jonas Wijne <jonas.wijne@jimber.org>
2021-02-09 10:54:16 +0100

    WIP

---
+++ backend/src/service/groupService.ts
@@ -0,0 +1,32 @@
+import Message from "../models/message";
+import {MessageBodyTypeInterface} from "../types";
+import {getChatById} from "./chatService";
+import Chat from "../models/chat";
+import {deleteChat, persistChat} from "./dataService";
+import {config} from "../config/config";
+import Contact from "../models/contact";
+import {sendEventToConnectedSockets} from "./socketService";
+
+export const handleGroupUpdate = (message: Message<{ contact: Contact, type: string }>, chat: Chat) => {
+    if (chat.adminId !== message.from) {
+        throw Error('not allowed')
+    }
+
+    switch (message.body.type) {
+        case "ADDUSER":
+            chat.contacts.push(message.body.contact)
+            break;
+        case "REMOVEUSER":
+            if (message.body.contact.id === config.userid){
+                deleteChat(<string>chat.chatId)
+                sendEventToConnectedSockets('chat_removed', chat.chatId)
+                return
+            }
+            chat.contacts = chat.contacts.filter(c => c.id !== message.body.contact.id)
+            break;
+        default:
+            throw Error('not implemented')
+    }
+    sendEventToConnectedSockets('chat_updated', chat)
+    persistChat(chat)
+};
