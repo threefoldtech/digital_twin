811b3ad24d0307bfa9de28951f1e146f7c030bff
tobiaschielens <tobias.chielens@gmail.org>
2021-02-10 15:56:59 +0100

    digitaltwin app id

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,8 +1,9 @@
 let userid = process.env.USER_ID || 'localhost:3000';
+let appId = process.env.DIGITALTWIN_APPID || "digitaltwin.jimbertesting.be"
 export const config = {
     appBackend: 'https://login.threefold.me',
     kycBackend: 'https://openkyc.live',
-    appId: `${userid}.digitaltwin.jimbertesting.be`,
+    appId: `${userid}.${appId}`,
     seedPhrase: 'calm science teach foil burst until next mango hole sponsor fold bottom cousin push focus track truly tornado turtle over tornado teach large fiscal',
     baseDir: '/appdata/',
     userid,
