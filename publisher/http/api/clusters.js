015b4307f9c49096bd91da9dbb469817854f5548
Maxime Daniel <root@maxux.net>
2021-02-15 17:37:25 +0100

    cluster: add staging cluster api endpoints
    Add endpoints:
     - GET /clusters       (list clusters)
     - GET /cluster/id     (get a cluster content)
     - PUT /cluster/id     (update or set a cluster content)
     - DELETE /cluster/id  (delete a cluster)

---
+++ publisher/http/api/clusters.js
@@ -0,0 +1,153 @@
+var express = require('express');
+var router = express.Router();
+var fs = require("fs").promises;
+var path = require('path');
+var app = express();
+
+const asyncHandler = require('express-async-handler')
+const clusterspath = "/tmp/clusters";
+
+async function clusters_list(cpath) {
+    const clusters = [];
+
+    let files = await fs.readdir(cpath)
+    let pathName = cpath;
+
+    const apath = path.resolve(pathName);
+
+    for(let file of files) {
+        /*
+        try {
+            let content = await fs.readFile(apath + '/' + file)
+            let data = JSON.parse(content);
+            clusters.push(data);
+
+        } catch(err) {
+            console.log(`${err}`);
+        }
+        */
+
+        clusters.push(file.substring(0, file.lastIndexOf('.')));
+    }
+
+    return clusters;
+}
+
+async function cluster_get(cpath) {
+    try {
+        await fs.access(cpath, fs.F_OK);
+
+    } catch(err) {
+        throw "invalid cluster";
+    }
+
+    try {
+        let content = await fs.readFile(cpath)
+        let data = JSON.parse(content);
+        return data;
+
+    } catch(err) {
+        console.log(`${err}`);
+    }
+}
+
+async function clusters_delete(cpath) {
+    try {
+        await fs.access(cpath, fs.F_OK);
+
+    } catch(err) {
+        throw "invalid cluster";
+    }
+
+    try {
+        fs.unlink(cpath);
+
+    } catch(err) {
+        throw "cannnot delete this cluster";
+    }
+
+    return {"status": "success"}
+}
+
+async function cluster_put(cpath, body) {
+    console.log(body);
+
+    if(!('name' in body))
+        throw "missing name";
+
+    if(!('psk' in body))
+        throw "missing pre-shared key (psk)";
+
+    if(!('ipv4' in body))
+        throw "missing ipv4 addresses list";
+
+    if(!('ipv6' in body))
+        throw "missing ipv6 addresses list";
+
+    try {
+        let write = await fs.writeFile(cpath, JSON.stringify(body));
+
+    } catch(err) {
+        console.log(`${err}`);
+    }
+
+    return {"status": "created"};
+}
+
+
+
+router.get('/clusters', function(req, res) {
+    clusters_list(clusterspath).then((content) => {
+        res.json(content);
+
+    }, (err) => {
+        res.status(422).json({
+            message: `${err}`
+        });
+    });
+})
+
+router.get('/cluster/:id', function(req, res) {
+    let fullpath = path.join(clusterspath, req.params.id + ".json");
+    const apath = path.resolve(fullpath);
+
+    cluster_get(apath).then((content) => {
+        res.json(content);
+
+    }, (err) => {
+        res.status(422).json({
+            message: `${err}`
+        });
+    });
+});
+
+router.delete('/cluster/:id', function(req, res) {
+    let fullpath = path.join(clusterspath, req.params.id + ".json");
+    const apath = path.resolve(fullpath);
+
+    clusters_delete(apath).then((content) => {
+        res.json(content);
+
+    }, (err) => {
+        res.status(422).json({
+            message: `${err}`
+        });
+    });
+});
+
+router.put('/cluster/:id', function(req, res) {
+    let fullpath = path.join(clusterspath, req.params.id + ".json");
+    const apath = path.resolve(fullpath);
+
+    cluster_put(apath, req.body).then((content) => {
+        res.json(content);
+
+    }, (err) => {
+        res.status(422).json({
+            message: `${err}`
+        });
+    });
+});
+
+
+module.exports = router
--- publisher/http/app.js
+++ publisher/http/app.js
@@ -3,6 +3,7 @@ const mustacheExpress = require('mustache-express');
 
 const cors = require('cors');
 const drive = require('./api/drive')
+const clusters = require('./api/clusters')
 const sites = require('./web/sites')
 
 
@@ -15,5 +16,6 @@ app.use(express.json());
 app.use(cors());
 app.use(sites);
 app.use(drive);
+app.use(clusters);
 
 module.exports = app
