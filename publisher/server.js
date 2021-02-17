ce6f30b0f2f2c6b02bdfed618bde87cb95891b2c
hamdy <hamdy.a.farag@gmail.com>
2021-02-17 14:19:11 +0200

    let's encrypt

--- publisher/server.js
+++ publisher/server.js
@@ -27,9 +27,24 @@ async function main(){
         })
     })
 
-    const server = app.listen(port, host, () => {
-        console.log(chalk.green(`✓ (HTTP Server) : http://${host}:${port}`));
+    
+    require('greenlock-express')
+    .init({
+        packageRoot: __dirname,
+
+        // contact for security and critical bug notices
+        maintainerEmail: "hamdy.a.farag@gmail.com",
+
+        // where to look for configuration
+        configDir: './greenlock.d',
+
+        // whether or not to run at cloudscale
+        cluster: false
     })
-}
+    // Serves on 80 and 443
+    // Get's SSL certificates magically!
+    .serve(app);
+
+    }
 
 main()
