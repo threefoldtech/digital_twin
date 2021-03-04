615e337bed630c461416b8e1565a3b0aa3ee326c
tobiaschielens <tobias.chielens@gmail.org>
2021-03-04 11:36:19 +0100

    fixed audio messages

--- frontend/src/components/MessageContentType/AudioContent.vue
+++ frontend/src/components/MessageContentType/AudioContent.vue
@@ -1,17 +1,28 @@
 <template>
-    {{ message.body }}
+    <audio
+        controls
+        class="max-w-full"
+        :src="calcExternalResourceLink(message.body.url)"
+        @load="$emit('scroll')"
+    ></audio>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
+import { calcExternalResourceLink } from '@/services/urlService';
 
-    export default defineComponent({
-        name: 'AudioContent',
-        props: {
-            message: { type: Object, required: true },
-        },
-        setup(props) {
-            return {};
-        },
-    });
+export default defineComponent({
+    name: 'AudioContent',
+    props: {
+        message: { type: Object, required: true },
+    },
+    setup(props) {
+        const url = computed(() => {
+            return;
+        });
+        return {
+            calcExternalResourceLink,
+        };
+    },
+});
 </script>
